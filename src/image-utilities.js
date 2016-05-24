cacf6142152b0ea543fbe3f5baeccb57b2281f81
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:21:30 -0400

    update to koco2

--- src/image-utilities.js
+++ src/image-utilities.js
@@ -1,66 +1,79 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['knockout', 'lodash'],
-    function(ko, _) {
-        'use strict';
+import ko from 'knockout';
+import _ from 'lodash';
 
-        var imageUtilities = {};
 
-        imageUtilities.defaultConcreteImageOptions = {
-            preferedWidth: 480,
-            preferedHeight: 270,
-            preferedRatio: '16:9',
-            defaultToClosestDimension: true
-        };
+var imageUtilities = {};
 
-        //scoop does not support float for widht & height
-        //scoop only works with width since cloudinary scale relatively
-        imageUtilities.getWidthTransformationFromCloudinaryUrl = function(url) {
-            var myArray = url.match(/\/w_\d+\/v1\//gi);
+imageUtilities.defaultConcreteImageOptions = {
+    preferedWidth: 480,
+    preferedHeight: 270,
+    preferedRatio: '16:9',
+    defaultToClosestDimension: true
+};
 
-            if (myArray && myArray.length) {
-                var width = myArray[0].match(/\d+/g)[0];
+//scoop does not support float for widht & height
+//scoop only works with width since cloudinary scale relatively
+imageUtilities.getWidthTransformationFromCloudinaryUrl = function(url) {
+    var myArray = url.match(/\/w_\d+\/v1\//gi);
 
-                if (width) {
-                    return parseInt(width);
-                }
-            }
+    if (myArray && myArray.length) {
+        var width = myArray[0].match(/\d+/g)[0];
 
-            return null;
-        };
+        if (width) {
+            return parseInt(width);
+        }
+    }
 
-        imageUtilities.updateCloudinaryUrlWidthTransformation = function(url, width) {
-            var result = url.replace('/v1/', "/w_" + width + '/v1/');
+    return null;
+};
 
-            return result;
-        };
+imageUtilities.updateCloudinaryUrlWidthTransformation = function(url, width) {
+    var result = url.replace('/v1/', "/w_" + width + '/v1/');
 
-        imageUtilities.isConceptualImage = function(image) {
-            return image && image.hasOwnProperty('concreteImages');
-        };
+    return result;
+};
 
-        //Attention: 2 type de DefaultConcreteImage (1 pour les previews de scoop et celui ci pour le default concrete image...)
-        //en ce moment on est chanceux, c'est la meme taille qui est utilisée par défaut mais il faudra ajuster si ca change
-        imageUtilities.getDefaultConcreteImage = function(conceptualImage) {
-            return imageUtilities.getConcreteImage(conceptualImage, imageUtilities.defaultConcreteImageOptions);
-        };
+imageUtilities.isConceptualImage = function(image) {
+    return image && image.hasOwnProperty('concreteImages');
+};
 
-        imageUtilities.getConcreteImage = function(conceptualImage, concreteImageOptions) {
-            var self = this;
-            concreteImageOptions = concreteImageOptions || self.defaultConcreteImageOptions;
+//Attention: 2 type de DefaultConcreteImage (1 pour les previews de scoop et celui ci pour le default concrete image...)
+//en ce moment on est chanceux, c'est la meme taille qui est utilisée par défaut mais il faudra ajuster si ca change
+imageUtilities.getDefaultConcreteImage = function(conceptualImage) {
+    return imageUtilities.getConcreteImage(conceptualImage, imageUtilities.defaultConcreteImageOptions);
+};
 
-            //concreteImageOptions = $.extend({}, imageUtilities.defaultConcreteImageOptions, concreteImageOptions);
+imageUtilities.getConcreteImage = function(conceptualImage, concreteImageOptions) {
+    var self = this;
+    concreteImageOptions = concreteImageOptions || self.defaultConcreteImageOptions;
 
-            //TODO: Work with concreteImageOptions instead of dimensionOrFunction
-            //TODO: Prendre en compte le cas ou on a un preferedHeight & preferedWidth mais pas de ratio... le cas existe :-(
+    //concreteImageOptions = $.extend({}, imageUtilities.defaultConcreteImageOptions, concreteImageOptions);
 
-            var concreteImage = null;
+    //TODO: Work with concreteImageOptions instead of dimensionOrFunction
+    //TODO: Prendre en compte le cas ou on a un preferedHeight & preferedWidth mais pas de ratio... le cas existe :-(
 
-            //Find perfect fit
-            if (concreteImageOptions.preferedWidth && concreteImageOptions.preferedHeight) {
+    var concreteImage = null;
+
+    //Find perfect fit
+    if (concreteImageOptions.preferedWidth && concreteImageOptions.preferedHeight) {
+        concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
+            return item.width == concreteImageOptions.preferedWidth && item.height == concreteImageOptions.preferedHeight;
+        });
+
+        if (concreteImage || !concreteImageOptions.defaultToClosestDimension) {
+            return concreteImage;
+        }
+    }
+
+    //Ratio + (width or height) - also perfect fit
+    if (concreteImageOptions.preferedRatio) {
+        if (concreteImageOptions.preferedWidth || concreteImageOptions.preferedHeight) {
+            if (concreteImageOptions.preferedWidth) {
                 concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                    return item.width == concreteImageOptions.preferedWidth && item.height == concreteImageOptions.preferedHeight;
+                    return item.width == concreteImageOptions.preferedWidth && item.dimensionRatio == concreteImageOptions.preferedRatio;
                 });
 
                 if (concreteImage || !concreteImageOptions.defaultToClosestDimension) {
@@ -68,90 +81,76 @@ define(['knockout', 'lodash'],
                 }
             }
 
-            //Ratio + (width or height) - also perfect fit
-            if (concreteImageOptions.preferedRatio) {
-                if (concreteImageOptions.preferedWidth || concreteImageOptions.preferedHeight) {
-                    if (concreteImageOptions.preferedWidth) {
-                        concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                            return item.width == concreteImageOptions.preferedWidth && item.dimensionRatio == concreteImageOptions.preferedRatio;
-                        });
-
-                        if (concreteImage || !concreteImageOptions.defaultToClosestDimension) {
-                            return concreteImage;
-                        }
-                    }
-
-                    if (concreteImageOptions.preferedHeight) {
-                        concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                            return item.height == concreteImageOptions.preferedHeight && item.dimensionRatio == concreteImageOptions.preferedRatio;
-                        });
-
-                        if (concreteImage || !concreteImageOptions.defaultToClosestDimension) {
-                            return concreteImage;
-                        }
-                    }
-                } else if (!concreteImageOptions.defaultToClosestDimension) {
-                    return ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                        return item.dimensionRatio == concreteImageOptions.preferedRatio;
-                    });
-                }
-            }
-
-            //Still no concrete image found
-            //No perfect fit & no defaultToClosestDimension
-            if (!concreteImageOptions.defaultToClosestDimension) {
-                return null;
-            }
-
-            if (concreteImageOptions.preferedRatio) {
-                var sameRatioConcreteImages = ko.utils.arrayFilter(conceptualImage.concreteImages, function(item) {
-                    return item.dimensionRatio == concreteImageOptions.preferedRatio;
+            if (concreteImageOptions.preferedHeight) {
+                concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
+                    return item.height == concreteImageOptions.preferedHeight && item.dimensionRatio == concreteImageOptions.preferedRatio;
                 });
 
-                if (sameRatioConcreteImages.length) {
-                    concreteImage = _.sortBy(sameRatioConcreteImages, function(item) {
-                        return item.width || 0;
-                    }).reverse()[0];
+                if (concreteImage || !concreteImageOptions.defaultToClosestDimension) {
+                    return concreteImage;
                 }
             }
-
-            if (!concreteImage) {
-                //return largest no matter the ratio
-                concreteImage = _.sortBy(conceptualImage.concreteImages, function(item) {
-                    return item.width || 0;
-                }).reverse()[0];
-            }
-
-            if (!concreteImage) {
-                concreteImage = null;
-            }
-
-            return concreteImage;
-        };
-
-        imageUtilities.getHighResolutionAudioVideoConcreteImage = function(conceptualImage) {
-            var concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                return item.mediaLink.href.match(/\/hr\//i);
+        } else if (!concreteImageOptions.defaultToClosestDimension) {
+            return ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
+                return item.dimensionRatio == concreteImageOptions.preferedRatio;
             });
+        }
+    }
 
-            if (!concreteImage) {
-                concreteImage = null;
-            }
+    //Still no concrete image found
+    //No perfect fit & no defaultToClosestDimension
+    if (!concreteImageOptions.defaultToClosestDimension) {
+        return null;
+    }
 
-            return concreteImage;
-        };
+    if (concreteImageOptions.preferedRatio) {
+        var sameRatioConcreteImages = ko.utils.arrayFilter(conceptualImage.concreteImages, function(item) {
+            return item.dimensionRatio == concreteImageOptions.preferedRatio;
+        });
 
-        imageUtilities.getLowResolutionAudioVideoConcreteImage = function(conceptualImage) {
-            var concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
-                return item.mediaLink.href.match(/\/br\//i);
-            });
+        if (sameRatioConcreteImages.length) {
+            concreteImage = _.sortBy(sameRatioConcreteImages, function(item) {
+                return item.width || 0;
+            }).reverse()[0];
+        }
+    }
 
-            if (!concreteImage) {
-                concreteImage = null;
-            }
+    if (!concreteImage) {
+        //return largest no matter the ratio
+        concreteImage = _.sortBy(conceptualImage.concreteImages, function(item) {
+            return item.width || 0;
+        }).reverse()[0];
+    }
 
-            return concreteImage;
-        };
+    if (!concreteImage) {
+        concreteImage = null;
+    }
 
-        return imageUtilities;
+    return concreteImage;
+};
+
+imageUtilities.getHighResolutionAudioVideoConcreteImage = function(conceptualImage) {
+    var concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
+        return item.mediaLink.href.match(/\/hr\//i);
     });
+
+    if (!concreteImage) {
+        concreteImage = null;
+    }
+
+    return concreteImage;
+};
+
+imageUtilities.getLowResolutionAudioVideoConcreteImage = function(conceptualImage) {
+    var concreteImage = ko.utils.arrayFirst(conceptualImage.concreteImages, function(item) {
+        return item.mediaLink.href.match(/\/br\//i);
+    });
+
+    if (!concreteImage) {
+        concreteImage = null;
+    }
+
+    return concreteImage;
+};
+
+export default imageUtilities;
